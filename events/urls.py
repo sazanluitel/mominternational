bc5bd2b4ac9816c6633fbde90f7d91684dbf3153
Prakash Luitel <prakashluitel991@gmail.com>
2024-07-11 08:27:37 +0545

    Deleted unused files

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,7 @@ from .views import FrontPage, AboutPage, ContactPage
 app_name = "events"
 
 urlpatterns = [
-    path("", FrontPage, name='front_page'),
+    path("", FrontPage.as_view(), name='front_page'),
     path("about/", AboutPage.as_view(), name='about_page'),
     path("contact/", ContactPage.as_view(), name='contact_page'),
 ]
